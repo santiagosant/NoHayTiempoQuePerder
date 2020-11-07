4e820f72d7499fd57e58736d6dba70f9eaef59cb
santiagosant <santidevargas@gmail.com>
2020-11-07 13:40:33 -0300

    Implementado: Timer contador de tiempo limite

---
+++ Assets/script/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public int tiempoInicial;
+    float tiempoActual;
+
+    public float escalaTiempo = -1;
+
+    public TMPro.TextMeshProUGUI timer;
+
+    void Start()
+    {
+        timer.text = tiempoInicial.ToString();
+        tiempoActual = tiempoInicial;
+    }
+
+    void Update()
+    {
+        tiempoActual = tiempoActual - Time.deltaTime;
+        timer.text = tiempoActual.ToString("f3");
+    }
+}
